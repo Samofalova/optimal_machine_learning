a0df7dd33963d20cfe5093c1dd7439c9e7f07d36
KirillKonstantinov <kikonstantinov@ozon.ru>
2022-04-08 17:48:15 +0300

    Add polynomial regression

--- HW4/regression.py
+++ HW4/regression.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.preprocessing import PolynomialFeatures
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -12,6 +13,10 @@ class InsufficientData(Exception):
 class LinearlyDependent(Exception):
     def __str__(self):
         return 'Присутствуют линейно зависимые признаки. Мы не можем применить МНК.'
+
+class DegreeError(Exception):
+    def __str__(self):
+        return 'Степень полинома должна быть целым неотрицательным числом.'
 
 
 def plot_3d_regression(X, y, coef, a0, n_point):
@@ -53,6 +58,45 @@ def exp_regression(X, y, tol=5, regularization=None, alpha=1.0, draw=False, n_po
         reg = Lasso(alpha=alpha).fit(X, y_new)
     elif regularization == 'L2':
         reg = Ridge(alpha=alpha).fit(X, y_new)
+    # Стьюдент
+    weights, bias = reg.coef_, np.exp(reg.intercept_)
+    if X.shape[1] == 2:
+        func = f'{round(bias, tol)}'
+        k = len(weights)
+        for i in range(k):
+            coef = weights[i]
+            func = func + f' * exp({round(coef, tol)}*x{k-i})'
+    else:
+        weights = weights[0]
+        bias = bias
+        func = f'{round(bias, tol)}' + f' * exp({round(weights, tol)}*x0)'
+    if draw == True and X.shape[1] > 2:
+        print('К сожалению, мы не можем построить график, так как размерность пространства признаков велика.')
+    elif draw == True and X.shape[1] == 2:
+        plot_3d_regression(X, y, weights, bias, n_point)
+    elif draw == True and X.shape[1] == 1:
+        plot_2d_regression(X, y, weights, bias, n_point)
+    return {'func': func, 
+            'weights': weights, 
+            'bias': bias}
+
+
+def poly_regression(X: pd.DataFrame, y: list, degree, regularization=None, alpha=1.0, draw=False, n_point=7000) -> dict:
+    if degree < 0 and int(degree) != float(degree):
+        raise DegreeError
+    
+    if X.shape[0] < 2**X.shape[1]:
+        raise InsufficientData
+    else:
+        X = X.to_numpy().reshape(-1, 1)
+        
+    if regularization is None:
+        reg = PolynomialFeatures(degree=degree).fit(X, y)
+    elif regularization == 'L1':
+        reg = Lasso(alpha=alpha)
+    elif regularization == 'L2':
+        reg = Ridge(alpha=alpha)
+    
     # Стьюдент
     weights, bias = reg.coef_, np.exp(reg.intercept_)
     if X.shape[1] == 2:
