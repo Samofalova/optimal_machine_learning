e807625cd9ec4c155b5d407ad3b0eecaa74e2124
KirillKonstantinov <kikonstantinov@ozon.ru>
2022-04-26 22:28:38 +0300

    Add doc and result ot return

--- HW5/log_barriers.py
+++ HW5/log_barriers.py
@@ -6,6 +6,29 @@ from copy import deepcopy
 
 
 def log_barriers(func: str, restrictions: list, start_point: tuple = tuple(), accuracy:float = 10**(-6), max_steps: int=500):
+    '''
+    Solving an optimisation problem for a function with equality-type
+    constraints by Newton's method (a way of solving the dual problem).
+    Returns the found point and the value of the function in it.
+    Parameters
+    ----------
+    func : string
+        Function for optimisation.
+    equality : list
+        List of strings with given linear constraints.
+    x0 : tuple
+        Starting point.
+    tol : int, default=5
+        The number of numbers after the point for rounding.
+    Examples
+    --------
+    >>> from HW5.log_barriers import *
+    >>> func = '(x1-2)**2 + x2**2'
+    >>> eqs = ['x1+x2-4 >= 0', '-4-2*x1+x2 >= 0']
+    >>> x, y = eq_dual_newton(func, eqs, (0, 0))
+    >>> x, y
+    (array([-0.,  4.]), 20.0000000000000)
+    '''
     tao = 1
     v = 10
     for i in range(len(restrictions)):
@@ -45,7 +68,7 @@ def log_barriers(func: str, restrictions: list, start_point: tuple = tuple(), ac
     
     res_new = sympify(func).subs(list(zip(symbs, next_point)))
     if type(res_new) == NaN:
-        return start_point, dataset
+        return np.array(start_point), res
         
     steps = 1
     while abs(res_new - res) > accuracy and max_steps > steps:
@@ -70,11 +93,11 @@ def log_barriers(func: str, restrictions: list, start_point: tuple = tuple(), ac
         res = deepcopy(res_new)
         res_new = sympify(func).subs(list(zip(symbs, next_point)))
         if type(res_new) == NaN:
-            return old_point
+            return np.array(old_point), res
 
         tao = tao*v
         steps += 1
         
 
-    return next_point
+    return np.array(next_point), res_new
 
