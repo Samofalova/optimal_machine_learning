e7db8cba2200a8dafba901b6e25adcbb4cbe00fc
Tatyana Samofalova <14.unicorn.14@gmail.com>
2022-05-10 21:38:59 +0300

    Add logistic_regression_with_rbf

--- HW6/code6.py
+++ HW6/code6.py
@@ -62,6 +62,50 @@ def own_logistic_regression(X_train, y_train, X_test,
            'y_hat': y_hat,
            'intercept': model.intercept_,
            'coef': model.coef_}
-    if X_poly.shape[1] == 2:
+    if X_poly.shape[1] == 2 and draw:
         draw_2d(X_test_poly, y_hat, model, poly)
+    return res
+
+
+def draw_rdf(X, y, clf):
+    colors = ['#' + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+              for col in range(len(np.unique(y_hat)))]
+    for clss in np.unique(y):
+        plt.scatter(X[y == clss, 0], X[y == clss, 1], 
+                    c=colors[clss], label=clss)
+    plt.xlabel("Признак 1")
+    plt.ylabel("Признак 2")
+    h = .02
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contour(xx, yy, Z, cmap=plt.cm.Paired)
+
+
+def logistic_regression_with_rbf(X_train, y_train, X_test,
+                                 C=1, gamma='scale',
+                                 draw=False, max_iter=100):
+    
+    str1 = 'Не выполняется условие gamma > 0'
+    str2 = 'Не выполняется условие C > 0'
+    str3 = 'Всего один класс, мы не можем обучить модель'
+    assert gamma == 'scale' or gamma > 0, str1
+    assert C > 0, str2
+    assert len(np.unique(y_train)) > 1, str3
+    model = SVC(kernel='rbf',
+                max_iter=max_iter,
+                C=C,
+                gamma=gamma)
+    model.fit(X_train, y_train)
+    y_hat = model.predict(X_test)
+    res = {'X_test': X_test,
+           'y_hat': y_hat,
+           'intercept': model.intercept_,
+           'coef': model.dual_coef_}
+    if X_test.shape[1] == 2 and draw:
+        draw_rdf(X_test, y_hat, model)
     return res
