9d0c20901c9d7b570733bbd660ae42d22a6eb2d8
Kirill Konstantinov <73214820+KirillKonstantinov@users.noreply.github.com>
2022-06-05 23:21:49 +0300

    Add full fucntion

--- HW7/branch_boundaries.py
+++ HW7/branch_boundaries.py
@@ -1,8 +1,9 @@
 import numpy as np
 from sympy import *
 from scipy.optimize import linprog
+from copy import deepcopy
 
-def branch_boundaries(F: str, constraint: list):
+def branche_boundaries(F: str, constraint: list):
     
     exp = sympify(F)
     
@@ -29,25 +30,106 @@ def branch_boundaries(F: str, constraint: list):
     b = []
     for ineq in constraint:
         ine = sympify(ineq)
-        b.append(float(ine.args[1]))
+        b.append(ine.args[1])
     
     b += [0]*len(symbs)
     
     res = linprog(c, A_ub=A, b_ub=b, method='simplex')['x']
     
-    if str(sum(res))[-1] == '0':
+    if all(str(i).split('.')[-1] == '0' for i in res) \
+            and all([sympify(const).subs(list(zip(symbs, res))) for const in constraint]):
         return res
-    else:
-        # ЗЛП-1
-        i = np.argmax(res%1)
-        whole = int(res[i])
-        lst = [0]*len(symbs)
-        lst[i] = 1
-        A.append(lst)
-        b.append(whole)
-        
-        # ЗЛП-2
-        i = np.argmax(res%1)
-        whole = -int(res[i])-1
-        ...
     
+    try:
+        res_zlp1 = ZLP1(F, constraint, res, symbs, A, b, c)
+    except:
+        res_zlp1 = None
+        
+    try:
+        res_zlp2 = ZLP2(F, constraint, res, symbs, A, b, c)
+    except:
+        res_zlp2 = None
+    
+    if res_zlp1 is not None and res_zlp2 is not None:
+        if exp.subs(list(zip(symbs, res_zlp1))) >= exp.subs(list(zip(symbs, res_zlp2))):
+            return res_zlp1, exp.subs(list(zip(symbs, res_zlp1)))
+        else:
+            return res_zlp2, exp.subs(list(zip(symbs, res_zlp2)))
+    elif res_zlp1 is not None and res_zlp2 is None:
+        return res_zlp1, exp.subs(list(zip(symbs, res_zlp1)))
+    elif res_zlp1 is None and res_zlp2 is not None:
+        return res_zlp2, exp.subs(list(zip(symbs, res_zlp2)))
+    else:
+        return None
+    
+def ZLP1(func, constraint, res_last, symbs, A, b, c):
+    i = np.argmax(res_last%1)
+    whole = int(res_last[i])
+    lst = [0]*len(symbs)
+    lst[i] = 1
+    
+    
+    A_new = deepcopy(A)
+    b_new = deepcopy(b)
+    A_new.append(lst)
+    b_new.append(whole)
+    #return c, A_new, b_new
+
+    res = linprog(c, A_ub=A_new, b_ub=b_new, method='simplex')['x']
+    
+    
+    if all(str(i).split('.')[-1] == '0' for i in res) \
+            and all([sympify(const).subs(list(zip(symbs, res))) for const in constraint]):
+        return res
+    
+    res_zlp3 = ZLP1(func, constraint, res, symbs, A_new, b_new, c)
+    res_zlp4 = ZLP2(func, constraint, res, symbs, A_new, b_new, c)
+    
+    
+    to_return = []
+    if all(str(i).split('.')[-1] == '0' for i in res_zlp3) \
+            and all([sympify(const).subs(list(zip(symbs, res_zlp3))) for const in constraint]):
+        to_return.append(res_zlp3)
+    if all(str(i).split('.')[-1] == '0' for i in res_zlp4) \
+            and all([sympify(const).subs(list(zip(symbs, res_zlp4))) for const in constraint]):
+        to_return.append(res_zlp4)
+        
+    if to_return:
+        return to_return
+    else:
+        return None
+
+def ZLP2(func, constraint, res_last, symbs, A, b, c):
+    i = np.argmax(res_last%1)
+    whole = -int(res_last[i])-1 
+    lst = [0]*len(symbs)
+    lst[i] = 1
+    
+    A_new = deepcopy(A)
+    b_new = deepcopy(b)
+    A_new.append(lst)
+    b_new.append(whole)
+
+    res = linprog(c, A_ub=A_new, b_ub=b_new, method='simplex')['x']
+    
+    if all(str(i).split('.')[-1] == '0' for i in res) \
+            and all([sympify(const).subs(list(zip(symbs, res))) for const in constraint]):
+        return res
+    
+    res_zlp5 = ZLP1(func, constraint, res, symbs, A_new, b_new, c)
+    res_zlp6 = ZLP2(func, constraint, res, symbs, A_new, b_new, c)
+    
+    to_return = []
+    if all(str(i).split('.')[-1] == '0' for i in res_zlp5) \
+            and all([sympify(const).subs(list(zip(symbs, res_zlp5))) for const in constraint]):
+        to_return.append(res_zlp5)
+    if all(str(i).split('.')[-1] == '0' for i in res_zlp5) \
+            and all([sympify(const).subs(list(zip(symbs, res_zlp5))) for const in constraint]):
+        to_return.append(res_zlp6)
+        
+    if to_return:
+        return to_return
+    else:
+        return None
+
+ 
