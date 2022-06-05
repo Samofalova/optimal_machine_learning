29ef2f298a81f9f3cbdbc4cf995395deda01eced
Tatyana Samofalova <14.unicorn.14@gmail.com>
2022-06-05 18:42:20 +0300

    Add doc HW6

--- HW6/Classification_2_classes.py
+++ HW6/Classification_2_classes.py
@@ -39,6 +39,45 @@ def own_logistic_regression(X_train, y_train, X_test,
                             degree: int=1, draw=False, max_iter=100):
     """
     A function that implements a two-class classification model based on logistic regression.
+    Parameters
+    ----------
+    X_train : numpy.array
+        A set of features for the training sample.
+    y_train : numpy.array
+        Set of class values for the training sample.
+    X_test : numpy.array
+        A set of features for the test sample.
+    penalty : string, default='none'
+        Type of regalarisation. ['l1', 'l2', 'elasticnet', 'none']
+    C : float, default=1
+        Regalarisation strength.
+    l1_ratio : float, default='none'
+        Regalarisation strength l1-regalarisation if penalty='elfsticnet' is selected.
+    degree : int, default=1
+        Polynomial degree.
+    draw : int, default=False
+        Plotting the classification graph.
+    max_iter : int, default=100
+        Number of learning iterations
+    Examples
+    --------
+    >>> from HW6.Classification_2_classes.py import *
+    >>> Xn, Yn = make_blobs(n_samples=50, centers=2, n_features=2, random_state=1, cluster_std=3)
+    >>> X_trainn, X_testn, y_trainn, y_testn = train_test_split(Xn, Yn, test_size=0.2)
+    >>> own_logistic_regression(X_trainn, y_trainn, X_testn)
+    {'X_test': array([[ -1.08681345,  10.70725528],
+        [ -9.5176013 ,  -1.32484178],
+        [-14.33005392,  -5.46674614],
+        [ -0.70244262,   3.65837874],
+        [ -9.40281334,  -3.59632261],
+        [ -3.44098628,  -8.14283755],
+        [ -3.72107801,   1.87087294],
+        [ -7.48076226,  -1.1600423 ],
+        [ -2.02823058,   1.59918157],
+        [ -2.17684453,   1.77291462]]),
+     'y_hat': array([0, 1, 1, 0, 1, 1, 0, 1, 0, 0]),
+     'intercept': array([-1.83565162]),
+     'coef': array([[-0.62953643, -0.91903067]])}
     """
     param = {'penalty': penalty, 
              'C': C, 
@@ -106,6 +145,44 @@ def logistic_regression_with_rbf(X_train, y_train, X_test,
     """
     A function implementing a two-class classification model based on logistic
     regression with radial basis functions.
+    Parameters
+    ----------
+    X_train : numpy.array
+        A set of features for the training sample.
+    y_train : numpy.array
+        Set of class values for the training sample.
+    X_test : numpy.array
+        A set of features for the test sample.
+    C : float, default=1
+        Regalarisation strength.
+    gamma : float, default='scale'
+        Kernel factor.
+    draw : int, default=False
+        Plotting the classification graph.
+    max_iter : int, default=100
+        Number of learning iterations
+    Examples
+    --------
+    >>> from HW6.Classification_2_classes.py import *
+    >>> Xn, Yn = make_blobs(n_samples=50, centers=2, n_features=2, random_state=1, cluster_std=3)
+    >>> X_trainn, X_testn, y_trainn, y_testn = train_test_split(Xn, Yn, test_size=0.2)
+    >>> logistic_regression_with_rbf(X_trainn, y_trainn, X_testn,
+                                     C=1, gamma='scale',
+                                     draw=True, max_iter=100)
+    {'X_test': array([[-12.26090633,  -0.19474408],
+        [ -3.72107801,   1.87087294],
+        [ -9.40281334,  -3.59632261],
+        [ -1.53291867,   6.15493551],
+        [ -0.75904895,   3.34974033],
+        [ -3.44098628,  -8.14283755],
+        [ -0.70244262,   3.65837874],
+        [ -2.23506656,   1.74360298],
+        [ -9.14095053,  -1.29792505],
+        [  2.72676391,  -1.77393226]]),
+    'y_hat': array([1, 0, 1, 0, 0, 1, 0, 0, 1, 0]),
+    'intercept': array([-0.12607821]),
+    'coef': array([[-0.32377093, -0.89114665, -0.2455269 , -1., -0.84723235,
+                    0.52369094,  1.,  0.51354647,  1.,  0.27043942]])}
     """
     str1 = 'Не выполняется условие gamma > 0'
     str2 = 'Не выполняется условие C > 0'
@@ -134,7 +211,42 @@ def svm_with_pddipm(X_train, y_train, X_test,
     """
     A function that implements a two-class classification model using
     the direct-double internal point method for the reference vector
-    method training problem
+    method training problem.
+    Parameters
+    ----------
+    X_train : numpy.array
+        A set of features for the training sample.
+    y_train : numpy.array
+        Set of class values for the training sample.
+    X_test : numpy.array
+        A set of features for the test sample.
+    penalty : string, default='none'
+        Type of regalarisation. ['l2', 'none']
+    C : float, default=1
+        Regalarisation strength.
+    draw : int, default=False
+        Plotting the classification graph.
+    max_iter : int, default=100
+        Number of learning iterations
+    Examples
+    --------
+    >>> from HW6.Classification_2_classes.py import *
+    >>> Xn, Yn = make_blobs(n_samples=50, centers=2, n_features=2, random_state=1, cluster_std=3)
+    >>> X_trainn, X_testn, y_trainn, y_testn = train_test_split(Xn, Yn, test_size=0.2)
+    >>> svm_with_pddipm(X_trainn, y_trainn, X_testn, max_iter=10500)
+    {'X_test': array([[ -9.40281334,  -3.59632261],
+        [-10.6243952 ,  -2.19347897],
+        [  3.31984663,   6.63262235],
+        [-12.00969936,  -2.82065719],
+        [  3.57487539,   2.12286917],
+        [ -0.70244262,   3.65837874],
+        [ -3.72107801,   1.87087294],
+        [ -8.45892304,  -4.84762705],
+        [ -2.17684453,   1.77291462],
+        [ -1.29908305,   6.2580992 ]]),
+    'y_hat': array([1, 1, 0, 1, 0, 0, 0, 1, 0, 0]),
+    'intercept': array([-1.16510368]),
+    'coef': array([[-0.3087416 , -0.28134706]])}
     """
     param = {'penalty': 'l2', 
              'C': C, 
