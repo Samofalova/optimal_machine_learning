655ad9216c7c987b38b895eb6da8c50a350c9322
Ksenia <99388976+MKJenna@users.noreply.github.com>
2022-06-18 18:39:53 +0300

    Delete unnecessary rows

--- Exam_Mosolova_.py
+++ Exam_Mosolova_.py
@@ -10,52 +10,51 @@ from sympy.core.numbers import NaN
 from copy import deepcopy
 
 
-def log_barriers(func: str, restrictions: list, start_point: tuple = tuple(), accuracy:float = 10**(-6), max_steps: int=500, tao = 1/1000):
+def log_barriers(func, inequality, start_point,
+                 accuracy=10**-6, max_steps=500,
+                 tao=0.001, v=2):
     '''
-    Solving an optimisation problem for a function with equality-type
-    constraints by log barriers method (a way of solving the dual problem).
+    Solving the optimization problem for a function with
+    inequality type constraints by the method of logarithmic
+    barriers (a method for solving a dual problem).
     Returns the found point and the value of the function in it.
     Parameters
     ----------
     func : string
         Function for optimisation.
-    restrictions : list
-        List of strings with given linear restrictions.
+    inequality : list
+        List of strings with given linear inequality.
     start_point : tuple
         Starting point.
-    accuracy : float, default=10**(-6)
+    accuracy : float, default=10**-6
         Tolerance for termination.
     max_steps : int, default=500
-        Maximum of iterations/
-    tao : default = 1/1000
-        Parameter.
+        Maximum of iterations.
+    tao : float, default = 0.001
+        Parameter for algorithm.
+    v : default = v
+        Parameter for algorithm.
+    Example
+    ----------
+    >>> f = '-x1+4*x2'
+    >>> c = ['3*x1-x2+6 >= 0', '-x1-2*x2+4 >= 0', 'x2+3 >= 0']
+    >>> res = log_barriers(f, c, start_point=(1, 1))
+    {'x': array([-1.14285714286459, 2.57142857143230], dtype=object),
+     'y': 11.4285714285938}
     '''
-    v = 2
     
-    for i in range(len(restrictions)):
-        if '>' in restrictions[i]:
-            restrictions[i] = restrictions[i][:restrictions[i].index('>')].replace(' ', '')
+    for i in range(len(inequality)):
+        if '>' in inequality[i]:
+            inequality[i] = inequality[i][:inequality[i].index('>')].replace(' ', '')
         else:
-            restrictions[i] = restrictions[i][:restrictions[i].index('<')].replace(' ', '')
+            inequality[i] = inequality[i][:inequality[i].index('<')].replace(' ', '')
 
     phi = f'{tao}*({func})'
-    for exp in restrictions:
+    for exp in inequality:
         phi += f' - log({exp})'
         
     X = Matrix([sympify(phi)])
     symbs = list(sympify(phi).free_symbols)
-
-    if len(start_point) == 0:
-        start_point = first_phase(restrictions, symbs)
-    if start_point == 'Введенные ограничения не могут использоваться вместе':
-        return start_point
-    elif start_point == 'Невозможно подобрать внутреннюю точку для данного метода':
-        return start_point
-
-    try:
-        res = sympify(func).subs(list(zip(symbs, start_point)))
-    except:
-        return 'Введена первоначальная точка, которая не удовлетворяет неравенствам'
     Y = Matrix(list(symbs))
     
     df = X.jacobian(Y).T
@@ -69,24 +68,12 @@ def log_barriers(func: str, restrictions: list, start_point: tuple = tuple(), ac
     next_point = [start_point[i]-xk[i] for i in range(len(start_point))]
     tao = tao*v
     
-    
+    res = sympify(func).subs(list(zip(symbs, start_point)))
     res_new = sympify(func).subs(list(zip(symbs, next_point)))
-    if type(res_new) == NaN:
-        return np.array(start_point), res
+
         
     steps = 1
     while abs(res_new - res) > accuracy and max_steps > steps:
-        phi = f'{tao}*({func})'
-        for exp in restrictions:
-            phi += f' - log({exp})'
-        
-        X = Matrix([sympify(phi)])
-        symbs = list(sympify(phi).free_symbols)
-        Y = Matrix(list(symbs))
-
-        df = X.jacobian(Y).T
-        ddf = df.jacobian(Y)
-
         lst_for_subs =  list(zip(symbs, next_point))
         dfx0 = df.subs(lst_for_subs)
         ddfx0 = ddf.subs(lst_for_subs)
@@ -96,9 +83,6 @@ def log_barriers(func: str, restrictions: list, start_point: tuple = tuple(), ac
         next_point = [next_point[i]+xk[i] for i in range(len(next_point))]
         res = deepcopy(res_new)
         res_new = sympify(func).subs(list(zip(symbs, next_point)))
-
-        if type(res_new) == NaN:
-            return np.array(old_point), res
 
         tao = tao*v
         steps += 1
