f06e1a8f131bb5f8852565ddf6f5f1dc07392e32
Tatyana Samofalova <14.unicorn.14@gmail.com>
2022-06-20 00:17:45 +0300

    update exam

--- Exam_Samofalova.py
+++ Exam_Samofalova.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sympy import *
 from sympy.core.numbers import NaN
-from .first_phase import first_phase
 from copy import deepcopy
 
 
@@ -50,11 +49,6 @@ def log_barriers(func, inequality, start_point,
         
     X = Matrix([sympify(phi)])
     symbs = list(sympify(phi).free_symbols)
-
-    try:
-        res = sympify(func).subs(list(zip(symbs, start_point)))
-    except:
-        return 'Введена первоначальная точка, которая не удовлетворяет неравенствам'
     Y = Matrix(list(symbs))
     
     df = X.jacobian(Y).T
@@ -68,36 +62,19 @@ def log_barriers(func, inequality, start_point,
     next_point = [start_point[i]-xk[i] for i in range(len(start_point))]
     tao = tao*v
     
-    
+    res = sympify(func).subs(list(zip(symbs, start_point)))
     res_new = sympify(func).subs(list(zip(symbs, next_point)))
-    if type(res_new) == NaN:
-        return np.array(start_point), res
         
     steps = 1
     while abs(res_new - res) > accuracy and max_steps > steps:
-        phi = f'{tao}*({func})'
-        for exp in inequality:
-            phi += f' - log({exp})'
-        
-        X = Matrix([sympify(phi)])
-        symbs = list(sympify(phi).free_symbols)
-        Y = Matrix(list(symbs))
-
-        df = X.jacobian(Y).T
-        ddf = df.jacobian(Y)
-
         lst_for_subs =  list(zip(symbs, next_point))
         dfx0 = df.subs(lst_for_subs)
         ddfx0 = ddf.subs(lst_for_subs)
 
         xk = ddfx0.inv() @ dfx0
-        old_point = deepcopy(next_point)
         next_point = [next_point[i]+xk[i] for i in range(len(next_point))]
         res = deepcopy(res_new)
         res_new = sympify(func).subs(list(zip(symbs, next_point)))
-
-        if type(res_new) == NaN:
-            return np.array(old_point), res
 
         tao = tao*v
         steps += 1
@@ -116,14 +93,20 @@ def log_barriers(func, inequality, start_point,
 # u = ['3*x1-x2+6 >= 0', '-x1-2*x2+4 >= 0', 'x2+3 >= 0']
 # res = log_barriers(func, u, start_point=(1, 1))
 
-# x_points = np.linspace(-5, 5)
-# y_points = np.linspace(-5, 5)
+# # x_points = np.linspace(-15, 15, 100)
+# # y_points = np.linspace(-15, 15, 100)
+# x_points = np.arange(-15, 15, 0.05)
+# y_points = np.arange(-15, 15, 0.05)
 # X, Y = np.meshgrid(x_points, y_points)
-# f = lambda x,y: -x + 4*y
-# Z = f(X, Y)
-# scatter = go.Scatter3d(x=[float(res['x'][0])],
-#                        y=[float(res['x'][1])],
-#                        z=[float(res['y'])],
+# f = lambda x,y: -x + 4*y if (-3*x+y-6 <= 0) and (-x-2*y+4 >= 0) and (y+3 >= 0) else None
+# Z = []
+# for i in range(len(X)):
+#     Z.append([])
+#     for j in range(len(X[i])):
+#         Z[-1].append(f(X[i][j], Y[i][j]))
+# scatter = go.Scatter3d(x=[round(float(res['x'][0]), 3)],
+#                        y=[round(float(res['x'][1]), 3)],
+#                        z=[round(float(res['y']), 3)],
 #                        marker={'size':3})
 # surface = go.Surface(z=Z, x=X, y=Y, colorscale='RdBu',
 #                      opacity=0.3,  showscale=True)
